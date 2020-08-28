8e654b70bead11667c58309f2697fda859d1bde9
YoelRoger <yoelfernandez2008@hotmail.com>
2020-08-28 14:51:09 -0300

    all

---
+++ seismology/api/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 -m venv .
+pip3 install -r requirements.txt
+source bin/activate
---
+++ seismology/api/main/resources/Sensor.py
@@ -0,0 +1,110 @@
+from flask_restful import Resource
+from flask import request, jsonify
+from .. import db
+from main.models import SensorModel
+from main.models import UserModel
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from main.auth.decorators import admin_required
+from main.map.Map import SensorSchema
+
+class Sensor(Resource):
+    @jwt_required
+    #obtener recurso
+    def get(self, id):
+        sensor = db.session.query(SensorModel).get_or_404(id)
+        return sensor_schema.jsonify(sensor)
+    @admin_required
+    #eliminar recurso
+    def delete(self, id):
+        sensor = db.session.query(SensorModel).get_or_404(id)
+        db.session.delete(sensor)
+        try:
+            db.session.commit()
+        except Exception as error:
+            db.session.rollback()
+            return '', 409
+        return "Sensor was deleted succesfully", 204
+    @admin_required
+    #modificar recurso
+    def put(self, id):
+        sensor = db.session.query(SensorModel).get_or_404(id)
+        data = sensor_schema.load(request.get_json())
+        for key, value in data:
+            setattr(sensor, key, value)
+        db.session.add(sensor)
+        try:
+            db.session.commit()
+            return sensor_schema.jsonify(sensor), 201
+        except Exception as error:
+            return str(error), 400
+
+
+class Sensors(Resource):
+    @jwt_required
+    #obtener lista de recursos
+    def get(self):
+        page = 1
+        per_page = 25
+        max_per_page = 50
+        #filtrar sensores
+        filters = request.get_json().items()
+        sensors = db.session.query(SensorModel)
+        try:
+            for key, value in filters:
+                if key == 'userId':
+                    sensors = sensors.filter(SensorModel.userId == value)
+                if key == 'active':
+                    sensors = sensors.filter(SensorModel.active == value)
+                if key == 'status':
+                    sensors = sensors.filter(SensorModel.status == value)
+                #Filtro user email
+                if key == 'user.email':
+                    sensors = sensors.join(SensorModel.user).filter(UserModel.email.like('%'+value+'%'))
+                # ORDENAMIENTO
+
+                if key == "sort_by":
+                    if value == "name.desc":
+                        sensors = sensors.order_by(SensorModel.name.desc())
+                    if value == "name.asc":
+                        sensors = sensors.order_by(SensorModel.name.asc())
+                    if value == "userId.desc":
+                        sensors = sensors.order_by(SensorModel.userId.desc())
+                    if value == "userId.asc":
+                        sensors = sensors.order_by(SensorModel.userId.asc())
+                    if value == "active.desc":
+                        sensors = sensors.order_by(SensorModel.active.desc())
+                    if value == "active.asc":
+                        sensors = sensors.order_by(SensorModel.active.asc())
+                    if value == "status.desc":
+                        sensors = sensors.order_by(SensorModel.status.desc())
+                    if value == "status.asc":
+                        sensors = sensors.order_by(SensorModel.status.asc())
+                #ORDENAMIENTO POR EMAIL
+                    if value == "user.email.desc":
+                        sensors = sensors.join(SensorModel.user).order_by(UserModel.email.desc())
+                    if value == "user.email.asc":
+                        sensors = sensors.join(SensorModel.user).order_by(UserModel.email.asc())
+
+                #PAGINACION
+                if key == "page":
+                    page = value
+                if key == "per_page":
+                    per_page = value
+        except:
+            pass
+
+        sensors = sensors.paginate(page, per_page, True, max_per_page)
+        return jsonify({'Sensors': [sensor.to_json() for sensor in sensors.items]})
+    @admin_required
+    #insertar recurso
+    def post(self):
+        data = sensor_schema.load(request.get_json())
+        sensor = SensorModel(name=data['name'], ip =data["ip"], port=data["port"], status=data["status"], active=data["active"], userId=data["userID"])
+        try:
+            db.session.add(sensor)
+            db.session.commit()
+        except Exception as error:
+            return str(error), 400
+        return sensor.to_json(), 201
+
+sensor_schema = SensorSchema()
